d24acfdcec45ea12f2e8892a9575d75387da3d52
Mr Jose Sanchez <leandrosanchez.it@gmail.com>
2022-03-01 13:48:40 -0400

    Dark mode ajustment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/layout/NavBar';
 import Spinner from './components/Spinner';
 import useFetch from './hooks/useFetch';
 import darkModeContext from './context/darkModeContext';
+import './app.css';
 
 function App() {
   
@@ -11,15 +12,27 @@ function App() {
   const { dark } = DarkModeContext;
 
   const URI = 'https://v2.jokeapi.dev/joke/Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
-  const URI_DARK = 'https://v2.jokeapi.dev/joke/Dark?type=single';
+  const URI_DARK = 'https://v2.jokeapi.dev/joke/Dark,Miscellaneous,Pun,Spooky,Christmas?type=single';
 
   const {data , loading, error, refetch } = useFetch(`${dark?URI_DARK:URI}`);
+
+  const warningAlert = <p className='alert alert-dark text-center'>Dark mode activated!</p>
+
+  if(dark) {
+    document.body.style = 'background: darkgrey'
+    setTimeout(() => {
+      
+    }, 2000);
+  } else {
+    document.body.style = 'background: transparent'
+  }
 
   if(error) console.log(error);
 
   return (
     <>
       <NavBar/>
+        {dark&&warningAlert}
       <div className='container'>
       {loading ?
       <Spinner/> :
